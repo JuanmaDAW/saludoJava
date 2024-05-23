5d76ee2295c4cf7811de603969142876985dfb68
dgalanf01 <169030606+dgalanf01@users.noreply.github.com>
2024-05-23 14:08:30 +0200

    Update Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -3,7 +3,7 @@ public class Saludo {
 	public static void main(String[] args) {
 		
 
-		System.out.println("Hola esto es la version 2 del programa de Saludo, mi nombre no es Abraham, sino Daniel");
+		System.out.println("eres una caca");
 
 
 	}
